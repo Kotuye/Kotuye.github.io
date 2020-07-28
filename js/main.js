0a3398476b974ec1b11058de21aa5591a619e16a
aapu <birojozsef650708@gmail.com>
2020-07-28 15:41:22 +0200

    refresh new Imput

---
+++ js/main.js
@@ -0,0 +1,43 @@
+
+/*Megrendelés összegének kiszámítása.
+
+
+menyiség input megkeresése a dokumentumban
+document.querySelector("input[name='amount-input']");
+
+létrehozom ---menyiség input megkeresése a dokumentumban--
+let amountInput = document.querySelector("input[name='amount-input']");
+
+mennyiség létrehozása
+let amount = 0;
+
+menyiség kiolvasása
+amountInput.value
+
+Egységár
+let price = 1200;
+
+Kiszámítás
+amount = parseInt(amountInput.value) * price;
+alul a létrehozott fügvény
+*/
+function calcAmount() {
+    let price = 1000;
+    let amountInput = document.querySelector("input[name='amount-input']");
+    let showAmount = document.querySelector("span.show-amount");
+    let amountNumber = parseInt(amountInput.value);
+    amountNumber = isNaN(amountNumber) ? 0 : amountNumber; 
+
+    if (amountNumber > 10) {
+        alert("Maximum 10 terméket vásárolhat!");
+    } else if (amountNumber < 1) {
+        alert("Minimum 1 terméket kell vásárolnia!")       
+    } else {
+        let amount = amountNumber * price;
+        showAmount.innerHTML = amount;
+
+    }
+
+
+
+}
